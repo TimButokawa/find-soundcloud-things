db6b2de66c4a059000e3521006d9ebbf1962a8aa
timbutokawa <tim@timbutorac.com>
2016-03-28 19:38:47 -0700

    Fix authorization issue for stream url
    Revise application config to intercept get responses to
    check for tracks data. Stream url requires client id as
    a param in order to gain access. This fix transforms the
    url and adds the id  as it comes in.

--- src/app/application.config.js
+++ src/app/application.config.js
@@ -5,7 +5,25 @@
         .module('soundCloudApi')
         .config(config);
 
-    function config(RestangularProvider) {
+    function config(RestangularProvider,
+                    CLIENT) {
         RestangularProvider.setBaseUrl('https://api.soundcloud.com/');
+        RestangularProvider.addResponseInterceptor(function(data, operation, what) {
+            return operation.match(/get/) ? _transformData(data, what) : data;
+        });
+
+        function _transformData(data, what) {
+            if(what === 'tracks') {
+                data = _addClientIdToStreamUrl(data);
+                return data;
+            }
+        }
+
+        function _addClientIdToStreamUrl(tracks) {
+            _.forEach(tracks, function(track) {
+                track.stream_url = track.stream_url +  '?client_id=' + CLIENT.ID;
+            });
+            return tracks;
+        }
     }
 })();
