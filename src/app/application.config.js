bc3c193031d36bec4dcee39a6ab89298774a4e5d
timbutokawa <tim@timbutorac.com>
2016-03-28 20:22:13 -0700

    Revise interceptor
    Fix data not being returned when accessing tracks/track
    conditionally create an array for a single track and
    append the client id to the stream url

--- src/app/application.config.js
+++ src/app/application.config.js
@@ -8,18 +8,20 @@
     function config(RestangularProvider,
                     CLIENT) {
         RestangularProvider.setBaseUrl('https://api.soundcloud.com/');
-        RestangularProvider.addResponseInterceptor(function(data, operation, what) {
-            return operation.match(/get/) ? _transformData(data, what) : data;
+        RestangularProvider.addResponseInterceptor(function(data, operation, what, url) {
+            return operation.match(/get/) ? _transformData(data, url) : data;
         });
 
-        function _transformData(data, what) {
-            if(what === 'tracks') {
+        function _transformData(data, url) {
+            if(url.match(/tracks/)) {
                 data = _addClientIdToStreamUrl(data);
                 return data;
             }
+            return data;
         }
 
         function _addClientIdToStreamUrl(tracks) {
+            tracks = tracks.length ? tracks : [tracks];
             _.forEach(tracks, function(track) {
                 track.stream_url = track.stream_url +  '?client_id=' + CLIENT.ID;
             });
