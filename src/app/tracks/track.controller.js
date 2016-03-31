548e27e11fa7f341e14fe78a8b9a8e85806aa7f5
timbutokawa <tim@timbutorac.com>
2016-03-31 08:34:10 -0700

    Update tracks controller to fetch a single tracks artwork

--- src/app/tracks/track.controller.js
+++ src/app/tracks/track.controller.js
@@ -5,13 +5,16 @@
         .module('soundCloudApi')
         .controller('TrackController', TrackController);
 
-    function TrackController(track) {
+    function TrackController(track,
+                             tracksArtworkService,
+                             IMAGE_CONFIG) {
         var vm = this;
 
         activate();
 
         function activate() {
             vm.track = track;
+            vm.track.artwork_url = tracksArtworkService.getArtworkForTrack(track, IMAGE_CONFIG.SIZE.LRG);
         }
     }
 })();
